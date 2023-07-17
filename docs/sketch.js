de329214b380656323dbee27a817116b3c770c44
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2023-07-18 06:27:37 +0900

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -196,7 +196,7 @@ function draw() {
   stroke(150);
   line(0,worldY[0]+65,width,worldY[0]+65);
 
-  if(-(worldX[0] - prorogueWidth/2) > textWidth(prorogue) + width) {
+  if(-(worldX[0] + prorogueWidth/2) > textWidth(story) + width) {
     if(exitButtonX > width / 2) {
       exitButtonX -= 3;
       exitButton.position(exitButtonX, height / 2 - exitButton.height / 2);
