2eac368fde97f5abdda86794388d5ad62cbfaddf
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2023-07-18 06:53:07 +0900

    ポップアップなし

--- docs/sketch.js
+++ docs/sketch.js
@@ -49,34 +49,6 @@ function setup() {
   }
   console.log(isSmartPhone());
 
-  window.addEventListener('load', function() {
-    if(!localStorage.getItem('disp_popup') && isSmartPhone()) {
-        localStorage.setItem('disp_popup', 'on');
-        const body = document.querySelector('body');
-        const bgPopup = document.querySelector('.bg_onetime_popup');
-        const popup = document.querySelector('.onetime_popup');
-        const popupTitleClose = document.querySelector('.onetime_popup_title_close');
-        body.classList.add('open_popup');
-
-        bgPopup.addEventListener('click', function() {
-            closePopup();
-        });
-        popup.addEventListener('click', function(e) {
-            e.stopPropagation();
-        });
-        popupTitleClose.addEventListener('click', function() {
-            closePopup();
-        });
-
-        function closePopup() {
-            body.classList.remove('open_popup');
-        }
-    }
-}, false);
-
-
-  
-
   const canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("canvas-container");
   textAlign(CENTER, CENTER);
