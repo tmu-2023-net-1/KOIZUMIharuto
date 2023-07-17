0e52ab410a51eab9c92430899f4839932a4b79aa
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2023-07-18 06:23:51 +0900

    exit

--- docs/sketch.js
+++ docs/sketch.js
@@ -2,10 +2,10 @@ let rotationRateX = 0;
 let rotationRateY = 0;
 let rotationRateZ = 0;
 let rotationButton;
+let exitButton;
 let layer = 1;
 var worldX = new Array(layer);
 var worldY = new Array(layer);
-let defWorldY;
 let touchIsDown = false;
 let enidingBool;
 let reverseEnding;
@@ -20,15 +20,16 @@ let curIndex = 0;
 let prorogue = "気がつくと廊下にいた。";
 let story = "真っ白な廊下に。少し弧を描くように曲がっているせいで遠く先は見えず、枝分かれもなければ扉もない、この延々と続く廊下を歩かざるを得なかった。そしてしばらく歩くうちに、気付いたことがある。どれだけ歩いたところで、疲労も空腹も眠気も感じないのだ。それと同時にいくら歩いても景色は変わらないのだが。これは夢だろうか？現実だとしたらどこなのだろうか?そろそろ1周したのではないだろうか?そんな事を考えながら進んでいるとふいに遠くから声が聞こえたような気がした。もう一度聞こえた。後ろからだ。振り返ってみると遠くに人影が見えた、、気がしたのだがまばたきと同時にその人影は消えていた。きっと疲れているのだろうと思いながらも再び歩き出す。やはり景色は変わらない。もう1周したのではないかと思ったその時、今度は前方に人影が見える。やはり遠いが自分と同じで歩いているように見える。今度は幻覚ではないことを祈りながら呼びかけてみる。聞こえていないのだろうか。もう一度呼びかけるとその人影は振り向いた、、気がしたのだがまばたきと同時にその人影は消えていた。きっと疲れているのだろう。それからしばらくの間また歩いていたがまた後ろから声が聞こえた。振り返ってみるとまた遠くに人影が見える。今度はまばたきをしても消えない、、と考えていたらいつの間にか消えていた。やはり幻覚。疲労は感じなくともここまで同じ景色が続くと精神的に疲れてくるわけだ。となれば考えても仕方がないし身体は疲れていないわけなのでまた歩き出す。もう何周もしたのではないだろうか。それにおそらく1周するごとに同じことを繰り返している気がする。後ろから声がしたと思えば消え、前に人影が見えたと思えば消える。そんな事を考えながら歩いていると、やはり今度は前方に人影が見えた。しかしこれまでとは違いこちらに向かって歩いてくる。まばたきをしても消えない、、と考えていてもその人影は消えず、着々と距離を縮めてくる。ようやく顔が見えそうな距離になった時、相手が「戻れ」と叫んできた。そしてそれと同時にその人は消えた。まばたきはしていない。確実に人であると視認できる距離で目を離していないのに消えたのだ。結局あの人は何者なのか、薄々分かったが確信はない。"
 let ending = "ただ、このままさらに進むのではなく戻ることにした。すると前方に人影が見えた。こちらに向かって歩いてくる。距離が近づくに連れここは現実世界ではないという仮説が確かなものになっていく。そして相手の顔が確かに見えた時、咄嗟に叫んだ。その直後、私は意識を失った。薄れゆく意識の中、引き返さなかったらどうなっていたのだろうと思いながら。";
-let prorogueWidth;
-let storyWidth;
-let endingsWidth;
+let prorogueWidth = 0;
+let storyWidth = 0;
+let endingsWidth = 0;
 
 let leftImg;
 let rightImg;
 let footprint;
 let footprintHeight;
 
+let exitButtonX = 0;
 const myURL = new URL(location.href);
 function preload(){
   // leftImg = loadImage("images/left.png");
@@ -46,11 +47,9 @@ function setup() {
   enidingBool = false;
   startTime = 0;
 
-  defWorldY = height / 2;
 
   prorogueWidth = textWidth(prorogue);
   storyWidth = textWidth(story);
-  endingsWidth = textWidth(ending);
   
  
   
@@ -59,6 +58,12 @@ function setup() {
   rotationButton.mousePressed(function() {
     requestMotionPermission();
     backgroundBool = true;
+  });
+  exitButton = createButton("抜け出す");
+  exitButtonX = width;
+  exitButton.position(exitButtonX, height / 2 - exitButton.height / 2);
+  exitButton.mousePressed(function() {
+    open('about:blank', '_self').close();
   });
   for(let i = 0; i < layer; i ++) {
     worldX[i] = width / 2;
@@ -94,11 +99,11 @@ function draw() {
 
   fill(0);
   controls();
-  
 
+  let speed = 200;
   if(backgroundBlack >= 255){
     if(!enidingBool) {
-      if( worldX[0] - prorogueWidth/2 + textWidth(prorogue) < width*3/4 || millis() - startTime > 100) {
+      if( worldX[0] - prorogueWidth/2 + textWidth(prorogue) < width*3/4 || millis() - startTime > speed) {
         if (curIndex < story.length) {
           prorogue = prorogue + story.substring(curIndex, curIndex+1);
           curIndex++;
@@ -111,7 +116,7 @@ function draw() {
         
       }
     }else{
-      if(worldX[0] - prorogueWidth/2 + textWidth(prorogue) - textWidth(reverseEnding) > width/4 || millis() - startTime > 100) {
+      if(worldX[0] - prorogueWidth/2 + textWidth(prorogue) - textWidth(reverseEnding) > width/4 || millis() - startTime > speed) {
         if (curIndex < ending.length) {
           if(reverseEnding == undefined) {
             reverseEnding = ending.substring(curIndex, curIndex+1);
@@ -145,14 +150,18 @@ function draw() {
 
   textSize(20);
 
-  textAlign(LEFT, TOP);
-  text(prorogue, worldX[0] - prorogueWidth/2, worldY[0]-75);
+  textAlign(LEFT, CENTER);
+  noStroke();
+  text(prorogue, worldX[0] - prorogueWidth/2, worldY[0]-100);
+  stroke(150);
+  line(0,worldY[0]-65,width,worldY[0]-65);
 
-  
+  let repeat = 5;
+  let positionY = 0;
   if (!enidingBool) {
-    let repeat = 5;
+    positionY = -25;
       if (curIndex % repeat == 0) {
-        storyWidth = textWidth(story.substring(0, curIndex));
+        storyWidth = textWidth(prorogue);
         if ((curIndex / repeat) % 2 == 0) {
           footprint = loadImage("images/left.png");
           footprintHeight = 10;
@@ -161,18 +170,39 @@ function draw() {
           footprintHeight = -10;
         }
       }
+  }else{
+    positionY = 25;
+      if (curIndex % repeat == 0) {
+        endingsWidth = textWidth(reverseEnding);
+        if ((curIndex / repeat) % 2 == 0) {
+          footprint = loadImage("images/left2.png");
+          footprintHeight = -10;
+        } else {
+          footprint = loadImage("images/right2.png");
+          footprintHeight = 10;
+        }
+      }
   }
+  imageMode(CENTER);
+  image(footprint, worldX[0] - prorogueWidth / 2 + storyWidth - endingsWidth, worldY[0] + positionY - footprintHeight, footprint.width / 4, footprint.height / 4);
 
-  image(footprint, worldX[0] + prorogueWidth / 2 + storyWidth, worldY[0] - 25 - footprintHeight, footprint.width / 4, footprint.height / 4);
+  noStroke();
+  textAlign(RIGHT, CENTER);
+  text(
+    reverseEnding,
+    worldX[0] - prorogueWidth/2 + textWidth(prorogue),
+    worldY[0] + 100
+  );
+  stroke(150);
+  line(0,worldY[0]+65,width,worldY[0]+65);
 
-  textAlign(RIGHT, TOP);
-    text(
-      reverseEnding,
-      worldX[0] - prorogueWidth/2 + textWidth(prorogue),
-      worldY[0] + 75
-    );
-  
-
+  if(-(worldX[0] - prorogueWidth/2) > textWidth(prorogue) + width) {
+    if(exitButtonX > width / 2) {
+      exitButtonX -= 3;
+      exitButton.position(exitButtonX, height / 2 - exitButton.height / 2);
+    }
+  }
+  // console.log(exitButton.position());
 }
 
 function handleMotion(event) {
@@ -214,9 +244,8 @@ function controls() {
     }
   }else{
     for(let i = 0; i < layer; i ++) {
-      worldX[i] += 4 * rotationRateX / (25 - 5 * i);
-      defWorldY += 4 * rotationRateZ / (25 - 5 * i);
-      worldY[i] = defWorldY + worldX[i]/300
+      worldX[i] += rotationRateX / (5 - i);
+      worldY[i] += rotationRateZ / (5 - i);
     }
   }
   if(keyIsDown(RIGHT_ARROW)) {
